3c4174b697e7c5931fcdeeab5494a904fb05818e
Mahadi Hasan <39445677+ratingminus@users.noreply.github.com>
2021-05-15 09:38:00 +0600

    Update minGW_compile.bat

--- minGW_compile.bat
+++ minGW_compile.bat
@@ -1,1 +1,1 @@
-g++ src/*.cpp -Iinclude -Llib -DOS2 -lmingw32 -lSDL2main -lSDL2 -llibfftw3-3 -o winexe/musico.exe
+g++.exe src/*.cpp -Iinclude -Llib -DOS2 -lmingw32 -lSDL2main -lSDL2 -llibfftw3-3 -o winexe/musico.exe
